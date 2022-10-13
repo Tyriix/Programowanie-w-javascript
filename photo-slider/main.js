3bb178bdeb19d119c5eb920ebb46fe8f5e639873
Tyriix <konrad.barszcz@microsoft.wsei.edu.pl>
2022-10-13 13:00:21 +0200

    Minor css and html changes, started slider script

--- photo-slider/main.js
+++ photo-slider/main.js
@@ -1,3 +1,17 @@
-setTimeout(function() => {
-    
-}, 2000);
+element = document.getElementById("one")
+var btnLeft = document.getElementById("btn-left")
+var btnRight = document.getElementById("btn-right")
+function animate() {
+if(element)
+{
+element.classList.add("slide-animation")
+}
+}
+
+
+setTimeout(animate, 2000);
+
+if(btnLeft && one && two && three && four && five && six)
+{
+    btnLeft.addEventListener("click", animate);
+}
