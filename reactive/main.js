b3c74bf1066dcf7812abd09442f7e1e46a16bfe5
Tyriix <konrad.barszcz@microsoft.wsei.edu.pl>
2022-11-10 13:13:13 +0100

    Reworked some functions

--- reactive/main.js
+++ reactive/main.js
@@ -1,12 +1,9 @@
-interval()
-
-const subBtn = document.getElementById("subscribe");
-const unSubBtn = document.getElementById("unsubscribe");
 
 
 class Observable {
     constructor(){
         this.observers = []
+        let timer = 1
     }
     subscribe(func){
         this.observers.push(func)
@@ -17,28 +14,23 @@ class Observable {
         this.observers = this.observers.filter(observer => observer != func)
     }
 
-    notify(data){
-        this.observers.forEach(observer => observer(data))
+
+    interval(timer) { //Publisher
+        setInterval(
+            () => {
+                this.observers.forEach(observer => observer(data))
+                timer++
+            }, 2000)
     }
 }
 
-function handleClick(){
-    observable.notify("Subscribed")
-}
 
-subBtn.addEventListener("click", observable.subscribe(logger))
-unSubBtn.addEventListener("click", observable.unsubscribe())
+const observable = new Observable()
+observable.subscribe(saveCToSessionStorage())
+observable.unsubscribe()
 
 
-function interval(data) { //Publisher
-    let timer = 1
-    setInterval(
-        () => {
-            saveCToSessionStorage(timer)
-            discoverPowerBallNumber(timer)
-            timer++
-        }, 2000)
-}
+
 
 function logger(data){
     console.log(data)
@@ -48,7 +40,6 @@ function saveCToSessionStorage(data) { //Sub
     console.log('[reader C]', data)
     const storageData = { data }
     sessionStorage.setItem('C', JSON.stringify(storageData))
-    Logger.log(data)
 }
 
 function discoverPowerBallNumber(data) { //Sub
