054b167a1fb492ffa04206d03d6fac9323a0924e
Konrad Barszcz <konrad.barszcz@microsoft.wsei.edu.pl>
2022-11-27 18:25:47 +0100

    Calculator final

--- calculator/main.js
+++ calculator/main.js
@@ -1,6 +1,6 @@
 
-var id = 1
 var div = document.getElementById("input-section");
+
 function createInput(){
 var input = document.createElement("input");
 var button = document.createElement("button");
@@ -8,36 +8,35 @@ var button = document.createElement("button");
 
 button.textContent = "Dodaj pole";
 input.style.marginTop = "15px";
-input.setAttribute("id", "input" + id);
+input.setAttribute("id", "input" + (document.getElementsByTagName("input").length + 1));
 input.setAttribute("class", "calcInput");
 div.appendChild(input);
-id++;
 }
 
 
 var resultDiv = document.getElementById("results");
-var inputTable = [];
 $(document).on('keyup', ".calcInput", function(e){
     var noChildren = div.querySelectorAll('#input-section .calcInput').length;;
-
     var sum = 0;
     var avg = 0;
     var max = 0;
-    var min = 0;
+    var min = this.value;
 
     var sumResult = document.getElementById("sum");
     var avgResult = document.getElementById("avg");
     var maxResult = document.getElementById("max");
     var minResult = document.getElementById("min"); 
+
     $('.calcInput').each(function(){
         sum += parseFloat(this.value);
 
         avg += parseFloat(this.value) / noChildren;
 
-        //console.log(sum);
-        //console.log(avg);
-        //console.log(noChildren);
+        max = Math.max(max, parseFloat(this.value));
+        min = Math.min(min, parseFloat(this.value));
         sumResult.innerText = sum;
         avgResult.innerText = avg;
+        maxResult.innerText = max;
+        minResult.innerText = min;
     })
 });
