aa8158ec4cf7282f63f7e9f715ed46b2e5a81b8d
Konrad Barszcz <konrad.barszcz@microsoft.wsei.edu.pl>
2023-01-18 19:16:56 +0100

    Added service worker to drumkit

--- Lab 3 - Drumkit/main.js
+++ Lab 3 - Drumkit/main.js
@@ -1,3 +1,29 @@
+document.addEventListener('DOMContentLoaded', appStart)
+
+function appStart() {
+    registerServiceWorker()
+    watchNetworkStatus()
+}
+function registerServiceWorker() {
+    navigator.serviceWorker
+        .register('service-worker-thread.js')
+        .then((registration) => {
+            console.log('[SW] Register', registration)
+            registration.sync.register('event test: qwerty')
+        })
+        .catch((err) => console.log(err))
+}
+function watchNetworkStatus() {
+    const statusContainer = document.querySelector('#net-status')
+    setInterval(
+        () => {
+            const status = navigator.onLine ? '<span id="status-online">Online</span>' : '<span id="status-offline">Offline!</span>'
+            statusContainer.innerHTML = 'Stan sieci: ' + status
+        }
+        , 1000)
+}
+
+
 document.addEventListener("keypress", onKeyPress);
 const startMetronomeBtn = document.querySelector(".start-button");
 const stopMetronomeBtn = document.querySelector(".stop-button");
---
+++ Lab 3 - Drumkit/service-worker-thread.js
@@ -0,0 +1,112 @@
+// lifecycle:
+// 1. 'install' event
+// 2. 'activate' event
+// 3. idle - serviceWorker czeka na eventy
+
+const cacheVersion = 'v3'
+const cacheFilesArray = [
+    'index.html',
+    'main.js',
+    'style.css',
+    './sounds/boom.wav',
+    './sounds/clap.wav',
+    './sounds/hihat.wav',
+    './sounds/kick.wav',
+    './sounds/openhat.wav',
+    './sounds/ride.wav',
+    './sounds/snare.wav',
+    './sounds/tink.wav',
+    './sounds/tom.wav'
+]
+
+const cacheAllRequests = true
+
+// pierwsza instalacja
+self.addEventListener('install',
+    (e) => {
+        console.log('[SW] install event', e)
+        // budujemy cache podczas instalacji service workera
+        e.waitUntil(
+            // CacheStorage
+            caches.open(cacheVersion)
+                .then(cache => {
+                    console.log('%c [SW] CACHING FILES', 'background: red; color: #fff, padding: 10px');
+                    cache.addAll(cacheFilesArray)
+                })
+        )
+    })
+
+// aktywowanie service workera
+self.addEventListener('activate',
+    (e) => {
+        console.log('[SW] activate event', e)
+        // sprawdzamy czy mamy aktualną wersję cache - wpis w caches.keys() (Promise)
+        e.waitUntil(
+            caches.keys()
+                .then(ck => {
+                    console.log('%c CACHE KEYS', 'background: red; color: #fff, padding: 10px', ck)
+                })
+        )
+
+    })
+
+// nasłuchiwanie na wiadomości
+self.addEventListener('message',
+    (e) => {
+        console.log('[SW] messge event', e)
+    })
+
+// słuchamy na fetch
+// fetch to zdarzenie generowane przy każdym requescie do serwera
+self.addEventListener('fetch',
+    e => {
+        // console.log('[SW] fetch event.request', e.request)
+        // tutaj następuje cache z użyciem Cache API
+        e.respondWith(
+            caches.match(e.request)
+                .then((resp) => {
+                    if (resp) {
+                        console.log('[SW] request from cache', resp);
+                        return resp
+                    }
+                    // e.request to stream, musimy go sklonować (stream "używa się" tylko raz)
+                    const req = e.request.clone()
+                    return fetchData(req)
+                })
+                .catch(function () {
+                    return caches.match('./sounds');
+                    // return caches.match('./img/drumkit.png');
+                })
+
+        )
+    })
+
+self.addEventListener('sync',
+    (e) => {
+        console.log('[SW] sync event', e)
+        // synchronizacja danych
+        // e.waitUntil (jakasFunkcja())
+    })
+
+// push to powiadomienie z serwera
+self.addEventListener('push',
+    (e) => {
+        console.log('[SW] fetch event', e)
+         self.registration.showNotification('SWorker notify!')
+    })
+
+function fetchData(req) {
+    return fetch(req)
+        .then(resp => {
+            console.log('RESP:', resp)
+            // response to stream object
+            let newResp = response.clone()
+            return caches.open(cacheVersion)
+        })
+        .then((cache) => {
+            if (resp.ok && cacheAllRequests) {
+                cache.put(resp.url, newResp)
+            }
+            return resp
+        })
+}
